bebd2e8d914ea03fd50de514c738454a2ac628b7
Jonas <jonas.costa@icloud.com>
2016-06-23 23:54:29 -0300

    pacote ServiceTelefone

---
+++ app/src/main/java/ServiceTelefone/NumeroTelefone.java
@@ -0,0 +1,98 @@
+package ServiceTelefone;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class NumeroTelefone {
+
+    private String valid;
+    private String number;
+    private String local_Format;
+    private String international_Format;
+    private String country_Prefix;
+    private String country_Code;
+    private String country_Name;
+    private String location;
+    private String carrier;
+    private String line_Type;
+
+    public String getValid() {
+        return valid;
+    }
+
+    public void setValid(String valid) {
+        this.valid = valid;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getLocal_Format() {
+        return local_Format;
+    }
+
+    public void setLocal_Format(String local_Format) {
+        this.local_Format = local_Format;
+    }
+
+    public String getInternational_Format() {
+        return international_Format;
+    }
+
+    public void setInternational_Format(String international_Format) {
+        this.international_Format = international_Format;
+    }
+
+    public String getCountry_Prefix() {
+        return country_Prefix;
+    }
+
+    public void setCountry_Prefix(String country_Prefix) {
+        this.country_Prefix = country_Prefix;
+    }
+
+    public String getCountry_Code() {
+        return country_Code;
+    }
+
+    public void setCountry_Code(String country_Code) {
+        this.country_Code = country_Code;
+    }
+
+    public String getCountry_Name() {
+        return country_Name;
+    }
+
+    public void setCountry_Name(String country_Name) {
+        this.country_Name = country_Name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getLine_Type() {
+        return line_Type;
+    }
+
+    public void setLine_Type(String line_Type) {
+        this.line_Type = line_Type;
+    }
+}
---
+++ app/src/main/java/ServiceTelefone/NumeroTelefoneDAO.java
@@ -0,0 +1,16 @@
+package ServiceTelefone;
+
+import android.widget.EditText;
+
+/**
+ * Created by jonascosta on 23/06/16.
+ */
+public class NumeroTelefoneDAO {
+
+    public boolean formatoValido(EditText numero){
+
+        String url = "http://apilayer.net/api/validate?access_key=571f5ff4394f11f35f34cf153167da64&number=555191039301&country_code=&format=1";
+
+        return false;
+    }
+}
